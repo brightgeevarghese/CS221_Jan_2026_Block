1659fefd88e324de76467a9f1b1b32882175e58a
Bright Gee Varghese <bvarghese@miu.edu>
2026-01-13 12:06:13 -0600

    Initial commit

--- src/lesson2/arraylist_impl/MyArrayList.java
+++ src/lesson2/arraylist_impl/MyArrayList.java
@@ -21,10 +21,47 @@ public class MyArrayList<E> {
         return true;
     }
 
+    public void add(int index, E data) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        if (size == capacity) {
+            reallocate();
+        }
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = data;
+        size++;
+    }
+
     private void reallocate() {
+//        System.out.println("Reallocating...");
         E[] newElements = (E[]) new Object[capacity * 2];
         System.arraycopy(elements, 0, newElements, 0, size);
         elements = newElements;
+        capacity *= 2;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        E removed = elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        elements[--size] = null;
+        return removed;
+    }
+
+    public E set(int index, E data) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        E oldData = elements[index];
+        elements[index] = data;
+        return oldData;
     }
 
     @Override
@@ -49,6 +86,18 @@ class Main {
         list.add(30);
         System.out.println(list);
         list.add(40);
+        System.out.println(list);
+        list.add(0, 50);
+        System.out.println(list);
+        list.add(4, 60);
+        System.out.println(list);
+        System.out.println(list.size());
+        list.add(3, 70);
+        System.out.println(list);
+//        list.add(100, 23);
+        System.out.println("Remove data from index 0: " + list.remove(0));
+        System.out.println(list);
+        System.out.println(list.set(2, 80));
         System.out.println(list);
     }
 }
